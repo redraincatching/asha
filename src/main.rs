07b50c81fa99bb84c8c9de09c4e92daeac205154
redraincatching <redraincatching@disroot.org>
2024-11-14 13:48:00 +0000

    moved executables out of source folder

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use asha::{self, output_assembly, read_compiled};
 fn main() {
     println!("{:?}", std::env::current_dir());
 
-    let bytes = read_compiled("./src/executables/hello");
+    let bytes = read_compiled("./executables/hello");
     output_assembly(bytes);
 }
